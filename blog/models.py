ce3c2a38ddcf767e66a416dd2f6737a1d7fc997c
JotaDeK <johanstivenx@gmail.com>
2023-04-07 16:26:50 -0500

    Se mejora la interactividad del panel Admin

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db.models.fields import CharField, DateField, URLField
+from django.db.models.fields import CharField, DateField, URLField, TextField
 from django.db.models.fields.files import ImageField
 from datetime import date
 
@@ -17,7 +17,7 @@ class Project(models.Model):
 class Post(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=300)
-    content = CharField(max_length=5000, null=True)
+    content = TextField(max_length=5000, null=True)
     image = ImageField(upload_to="blog/images/blog")
     date = DateField(default=date.today)
 
