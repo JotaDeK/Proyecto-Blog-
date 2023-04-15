9befb5c9cf4af6e24c0385039c5e5231b1587382
JotaDeK <johanstivenx@gmail.com>
2023-04-15 18:20:42 -0500

    Se corrijen errores y se mejora la estetica de la NavBar

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django import forms
+from django.forms import TextInput, Textarea
+from django.utils.safestring import mark_safe
 from .models import Project, Post, Like
 list = [Post, Like]
 
